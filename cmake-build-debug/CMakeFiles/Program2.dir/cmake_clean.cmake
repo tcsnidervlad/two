e6a3399bb5241b7df53ce78b9d6baa9b86272af3
Влад <vvipvsv@gmail.com>
2022-02-14 19:20:27 +0300

    first commit

---
+++ cmake-build-debug/CMakeFiles/Program2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Program2.dir/main.c.obj"
+  "Program2.exe"
+  "Program2.exe.manifest"
+  "Program2.pdb"
+  "libProgram2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Program2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
